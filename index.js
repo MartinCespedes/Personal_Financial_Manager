de644d0b3c1515ef357143f9dc524ba04bd13b9e
MartinCespedes <martin.cespedes002@mymdc.net>
2023-04-09 12:27:12 -0400

    updated index.js with server connection

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Personal_Financial_Manager');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
